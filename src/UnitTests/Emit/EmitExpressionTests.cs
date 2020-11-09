415246d6804799581843b45a7bdbff631518781c
Marc Jacobi <marc.jacobi@macaw.nl>
2020-11-09 11:27:13 +0100

    emit expression tests

--- src/UnitTests/Emit/EmitExpressionTests.cs
+++ src/UnitTests/Emit/EmitExpressionTests.cs
@@ -9,7 +9,7 @@ namespace UnitTests.Emit
     public class EmitExpressionTests
     {
         [TestMethod]
-        public void ExpressionCompare()
+        public void ExpressionCompareEqual()
         {
             const string code =
                 "module test" + Tokens.NewLine +
@@ -23,7 +23,97 @@ namespace UnitTests.Emit
             var body = moduleClass.Methods.First().Body;
             body.Instructions.Should().HaveCount(5);
 
-            emit.SaveAs("ExpressionCompare.dll");
+            emit.SaveAs("ExpressionCompareEqual.dll");
+        }
+
+        [TestMethod]
+        public void ExpressionCompareGreater()
+        {
+            const string code =
+                "module test" + Tokens.NewLine +
+                "fn: ()" + Tokens.NewLine +
+                Tokens.Indent1 + "b = 42 > 101" + Tokens.NewLine
+                ;
+
+            var emit = Emit.Create(code);
+
+            var moduleClass = emit.Context.Module.Types.Find("test");
+            var body = moduleClass.Methods.First().Body;
+            body.Instructions.Should().HaveCount(5);
+
+            emit.SaveAs("ExpressionCompareGreater.dll");
+        }
+
+        [TestMethod]
+        public void ExpressionCompareLesser()
+        {
+            const string code =
+                "module test" + Tokens.NewLine +
+                "fn: ()" + Tokens.NewLine +
+                Tokens.Indent1 + "b = 42 < 101" + Tokens.NewLine
+                ;
+
+            var emit = Emit.Create(code);
+
+            var moduleClass = emit.Context.Module.Types.Find("test");
+            var body = moduleClass.Methods.First().Body;
+            body.Instructions.Should().HaveCount(5);
+
+            emit.SaveAs("ExpressionCompareLesser.dll");
+        }
+
+        [TestMethod]
+        public void ExpressionCompareNotEqual()
+        {
+            const string code =
+                "module test" + Tokens.NewLine +
+                "fn: ()" + Tokens.NewLine +
+                Tokens.Indent1 + "b = 42 <> 101" + Tokens.NewLine
+                ;
+
+            var emit = Emit.Create(code);
+
+            var moduleClass = emit.Context.Module.Types.Find("test");
+            var body = moduleClass.Methods.First().Body;
+            body.Instructions.Should().HaveCount(7);
+
+            emit.SaveAs("ExpressionCompareNotEqual.dll");
+        }
+
+        [TestMethod]
+        public void ExpressionCompareGreaterEqual()
+        {
+            const string code =
+                "module test" + Tokens.NewLine +
+                "fn: ()" + Tokens.NewLine +
+                Tokens.Indent1 + "b = 42 >= 101" + Tokens.NewLine
+                ;
+
+            var emit = Emit.Create(code);
+
+            var moduleClass = emit.Context.Module.Types.Find("test");
+            var body = moduleClass.Methods.First().Body;
+            body.Instructions.Should().HaveCount(7);
+
+            emit.SaveAs("ExpressionCompareGreaterEqual.dll");
+        }
+
+        [TestMethod]
+        public void ExpressionCompareLesserEqual()
+        {
+            const string code =
+                "module test" + Tokens.NewLine +
+                "fn: ()" + Tokens.NewLine +
+                Tokens.Indent1 + "b = 42 <= 101" + Tokens.NewLine
+                ;
+
+            var emit = Emit.Create(code);
+
+            var moduleClass = emit.Context.Module.Types.Find("test");
+            var body = moduleClass.Methods.First().Body;
+            body.Instructions.Should().HaveCount(7);
+
+            emit.SaveAs("ExpressionCompareLesserEqual.dll");
         }
     }
 }
