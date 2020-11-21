d7a64f69b719fa45128be84e44618f9f060c9f5c
Marc Jacobi <marc.jacobi@macaw.nl>
2020-11-21 11:58:19 +0100

    fix static intrinsic functions

--- src/Zsharp/AST/AstFunctionDefinitionIntrinsic.cs
+++ src/Zsharp/AST/AstFunctionDefinitionIntrinsic.cs
@@ -4,19 +4,6 @@
     {
         public static readonly AstIdentifierIntrinsic SourceParameterIdentifier = new AstIdentifierIntrinsic("source", AstIdentifierType.Parameter);
 
-        public static readonly AstFunctionDefinitionIntrinsic ConvertU8ToU16 = new AstFunctionDefinitionIntrinsic(
-            AstIdentifierIntrinsic.U16, AstTypeDefinitionIntrinsic.U8, AstTypeDefinitionIntrinsic.U16);
-        public static readonly AstFunctionDefinitionIntrinsic ConvertU8ToU32 = new AstFunctionDefinitionIntrinsic(
-            AstIdentifierIntrinsic.U32, AstTypeDefinitionIntrinsic.U8, AstTypeDefinitionIntrinsic.U32);
-        public static readonly AstFunctionDefinitionIntrinsic ConvertU16ToU32 = new AstFunctionDefinitionIntrinsic(
-            AstIdentifierIntrinsic.U32, AstTypeDefinitionIntrinsic.U16, AstTypeDefinitionIntrinsic.U32);
-        public static readonly AstFunctionDefinitionIntrinsic ConvertU8ToU64 = new AstFunctionDefinitionIntrinsic(
-            AstIdentifierIntrinsic.U64, AstTypeDefinitionIntrinsic.U8, AstTypeDefinitionIntrinsic.U64);
-        public static readonly AstFunctionDefinitionIntrinsic ConvertU16ToU64 = new AstFunctionDefinitionIntrinsic(
-            AstIdentifierIntrinsic.U64, AstTypeDefinitionIntrinsic.U16, AstTypeDefinitionIntrinsic.U64);
-        public static readonly AstFunctionDefinitionIntrinsic ConvertU32ToU64 = new AstFunctionDefinitionIntrinsic(
-            AstIdentifierIntrinsic.U64, AstTypeDefinitionIntrinsic.U32, AstTypeDefinitionIntrinsic.U64);
-
         public AstFunctionDefinitionIntrinsic(AstIdentifierIntrinsic typeIdentifier, AstTypeDefinitionIntrinsic fromParameter, AstTypeDefinitionIntrinsic toReturn)
         {
             SetIdentifier(typeIdentifier);
@@ -33,12 +20,25 @@
 
         public static void AddAll(AstSymbolTable symbols)
         {
-            AddIntrinsicSymbol(symbols, AstFunctionDefinitionIntrinsic.ConvertU8ToU16);
-            AddIntrinsicSymbol(symbols, AstFunctionDefinitionIntrinsic.ConvertU8ToU32);
-            AddIntrinsicSymbol(symbols, AstFunctionDefinitionIntrinsic.ConvertU16ToU32);
-            AddIntrinsicSymbol(symbols, AstFunctionDefinitionIntrinsic.ConvertU8ToU64);
-            AddIntrinsicSymbol(symbols, AstFunctionDefinitionIntrinsic.ConvertU16ToU64);
-            AddIntrinsicSymbol(symbols, AstFunctionDefinitionIntrinsic.ConvertU32ToU64);
+            var convertU8ToU16 = new AstFunctionDefinitionIntrinsic(
+                AstIdentifierIntrinsic.U16, AstTypeDefinitionIntrinsic.U8, AstTypeDefinitionIntrinsic.U16);
+            var convertU8ToU32 = new AstFunctionDefinitionIntrinsic(
+                AstIdentifierIntrinsic.U32, AstTypeDefinitionIntrinsic.U8, AstTypeDefinitionIntrinsic.U32);
+            var convertU16ToU32 = new AstFunctionDefinitionIntrinsic(
+                AstIdentifierIntrinsic.U32, AstTypeDefinitionIntrinsic.U16, AstTypeDefinitionIntrinsic.U32);
+            var convertU8ToU64 = new AstFunctionDefinitionIntrinsic(
+                AstIdentifierIntrinsic.U64, AstTypeDefinitionIntrinsic.U8, AstTypeDefinitionIntrinsic.U64);
+            var convertU16ToU64 = new AstFunctionDefinitionIntrinsic(
+                AstIdentifierIntrinsic.U64, AstTypeDefinitionIntrinsic.U16, AstTypeDefinitionIntrinsic.U64);
+            var convertU32ToU64 = new AstFunctionDefinitionIntrinsic(
+                AstIdentifierIntrinsic.U64, AstTypeDefinitionIntrinsic.U32, AstTypeDefinitionIntrinsic.U64);
+
+            AddIntrinsicSymbol(symbols, convertU8ToU16);
+            AddIntrinsicSymbol(symbols, convertU8ToU32);
+            AddIntrinsicSymbol(symbols, convertU16ToU32);
+            AddIntrinsicSymbol(symbols, convertU8ToU64);
+            AddIntrinsicSymbol(symbols, convertU16ToU64);
+            AddIntrinsicSymbol(symbols, convertU32ToU64);
         }
 
         private static void AddIntrinsicSymbol(AstSymbolTable symbols, AstFunctionDefinitionIntrinsic function)
