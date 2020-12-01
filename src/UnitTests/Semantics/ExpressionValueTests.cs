ddb893c79a4f799cfbdaa85c92baaf34e9f1ded5
Marc Jacobi <marc.jacobi@macaw.nl>
2020-12-01 07:49:26 +0100

    expression value tests

---
+++ src/UnitTests/Semantics/ExpressionValueTests.cs
@@ -0,0 +1,157 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zsharp.AST;
+using Zsharp.Semantics;
+
+namespace UnitTests.Semantics
+{
+    [TestClass]
+    public class ExpressionValueTests
+    {
+        [TestMethod]
+        public void LiteralNumeric()
+        {
+            var expression = new AstExpression(
+                new AstExpressionOperand(
+                    new AstLiteralNumeric(42)))
+            {
+                Operator = AstExpressionOperator.Number
+            };
+
+            var constValue = new ExpressionValue();
+            expression.Accept(constValue);
+
+            constValue.ValueInt.Should().Be(42);
+        }
+
+        [TestMethod]
+        public void LiteralNumericPlus()
+        {
+            var expression = new AstExpression(
+                new AstExpressionOperand(
+                    new AstLiteralNumeric(42)))
+            {
+                Operator = AstExpressionOperator.Plus
+            };
+
+            expression.Add(new AstExpressionOperand(
+                new AstLiteralNumeric(101)));
+
+            var constValue = new ExpressionValue();
+            expression.Accept(constValue);
+
+            constValue.HasValue.Should().BeTrue();
+            constValue.ValueInt.Should().Be(42 + 101);
+        }
+
+        [TestMethod]
+        public void LiteralNumericMinus()
+        {
+            var expression = new AstExpression(
+                new AstExpressionOperand(
+                    new AstLiteralNumeric(42)))
+            {
+                Operator = AstExpressionOperator.Minus
+            };
+
+            expression.Add(new AstExpressionOperand(
+                new AstLiteralNumeric(101)));
+
+            var constValue = new ExpressionValue();
+            expression.Accept(constValue);
+
+            constValue.HasValue.Should().BeTrue();
+            constValue.ValueInt.Should().Be(42 - 101);
+        }
+
+        [TestMethod]
+        public void LiteralNumericMultiply()
+        {
+            var expression = new AstExpression(
+                new AstExpressionOperand(
+                    new AstLiteralNumeric(42)))
+            {
+                Operator = AstExpressionOperator.Multiply
+            };
+
+            expression.Add(new AstExpressionOperand(
+                new AstLiteralNumeric(101)));
+
+            var constValue = new ExpressionValue();
+            expression.Accept(constValue);
+
+            constValue.HasValue.Should().BeTrue();
+            constValue.ValueInt.Should().Be(42 * 101);
+        }
+
+        [TestMethod]
+        public void LiteralNumericDivide()
+        {
+            var expression = new AstExpression(
+                new AstExpressionOperand(
+                    new AstLiteralNumeric(101)))
+            {
+                Operator = AstExpressionOperator.Divide
+            };
+
+            expression.Add(new AstExpressionOperand(
+                new AstLiteralNumeric(42)));
+
+            var constValue = new ExpressionValue();
+            expression.Accept(constValue);
+
+            constValue.HasValue.Should().BeTrue();
+            constValue.ValueInt.Should().Be(101 / 42);
+        }
+
+        [TestMethod]
+        public void LiteralNumericEqual()
+        {
+            var expression = new AstExpression(
+                new AstExpressionOperand(
+                    new AstLiteralNumeric(101)))
+            {
+                Operator = AstExpressionOperator.Equal
+            };
+
+            expression.Add(new AstExpressionOperand(
+                new AstLiteralNumeric(42)));
+
+            var constValue = new ExpressionValue();
+            expression.Accept(constValue);
+
+            constValue.HasValue.Should().BeTrue();
+            constValue.ValueBool.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void LiteralNumericPlusEqual()
+        {
+            // 42 + 101 == 143
+            var expression = new AstExpression(
+                new AstExpressionOperand(
+                    new AstLiteralNumeric(42)))
+            {
+                Operator = AstExpressionOperator.Plus
+            };
+
+            expression.Add(new AstExpressionOperand(
+                new AstLiteralNumeric(101)));
+
+            expression = new AstExpression(
+                new AstExpressionOperand(expression))
+            {
+                Operator = AstExpressionOperator.Equal
+            };
+
+            expression.Add(new AstExpressionOperand(
+                new AstLiteralNumeric(143)));
+
+            var constValue = new ExpressionValue();
+            expression.Accept(constValue);
+
+            constValue.HasValue.Should().BeTrue();
+            constValue.ValueBool.Should().BeTrue();
+        }
+    }
+}
--- src/Zsharp/Semantics/ExpressionValue.cs
+++ src/Zsharp/Semantics/ExpressionValue.cs
@@ -6,43 +6,77 @@ namespace Zsharp.Semantics
 {
     public static class ExpressionExtensions
     {
-        public static UInt64 ConstantValue(this AstExpression expression)
+        public static Int64 ConstantValue(this AstExpression expression)
         {
             var visitor = new ExpressionValue();
             expression.Accept(visitor);
-            return visitor.Value;
+            return visitor.ValueInt.GetValueOrDefault();
         }
     }
 
     public class ExpressionValue : AstVisitor
     {
-        private readonly Stack<UInt64> _values = new Stack<UInt64>();
+        private readonly Stack<object> _values = new Stack<object>();
 
-        public UInt64 Value
+        private Int64 PopInt()
+            => (Int64)_values.Pop();
+
+        private bool PopBool()
+            => (bool)_values.Pop();
+
+        public bool HasValue => _values.Count == 1;
+
+        public Int64? ValueInt
         {
-            get { return _values.Peek(); }
+            get
+            {
+                var val = _values.Peek();
+                return val is Int64 ? (Int64)val : (Int64?)null;
+            }
+        }
+
+        public bool? ValueBool
+        {
+            get
+            {
+                var val = _values.Peek();
+                return val is bool ? (bool)val : (bool?)null;
+            }
         }
 
         public override void VisitExpression(AstExpression expression)
         {
             VisitChildren(expression);
 
-            UInt64 lhs = 0;
-            if (expression.LHS != null)
+            Ast.Guard(expression.RHS, "Invalid Expression: No Right-Hand-Side.");
+
+            object value;
+            if ((expression.Operator & AstExpressionOperator.MaskComparison) > 0)
             {
-                lhs = _values.Pop();
+                Int64 rhs = PopInt();
+                Int64 lhs = expression.LHS != null ? PopInt() : 0;
+
+                value = PerformComparisonOperation(lhs, expression.Operator, rhs);
             }
-            if (expression.RHS == null)
+            else if ((expression.Operator & AstExpressionOperator.MaskLogic) > 0)
             {
-                throw new InvalidOperationException("Invalid Expression: No Right-Hand-Side.");
+                bool rhs = PopBool();
+                bool lhs = expression.LHS != null && PopBool();
+
+                value = PerformLogicalOperation(lhs, expression.Operator, rhs);
+            }
+            else // arithmetic and bitwise
+            {
+                Int64 rhs = PopInt();
+                Int64 lhs = expression.LHS != null ? PopInt() : 0;
+
+                value = PerformNumericalOperation(lhs, expression.Operator, rhs);
             }
 
-            UInt64 rhs = _values.Pop();
-
-            _values.Push(PerformOperation(lhs, expression.Operator, rhs));
+            _values.Push(value);
         }
 
-        private ulong PerformOperation(ulong lhs, AstExpressionOperator op, ulong rhs)
+        private Int64 PerformNumericalOperation(Int64 lhs, AstExpressionOperator op, Int64 rhs)
         {
             return op switch
             {
@@ -53,7 +87,43 @@ namespace Zsharp.Semantics
                 AstExpressionOperator.Divide => lhs / rhs,
                 AstExpressionOperator.Modulo => lhs % rhs,
                 AstExpressionOperator.Power => lhs ^ rhs,
-                _ => throw new NotImplementedException($"Operator {op} not implemented.")
+                AstExpressionOperator.Negate => -rhs,
+
+                AstExpressionOperator.BitAnd => lhs & rhs,
+                AstExpressionOperator.BitOr => lhs | rhs,
+                AstExpressionOperator.BitNegate => ~rhs,
+                AstExpressionOperator.BitXor => lhs ^ rhs,
+                AstExpressionOperator.BitShiftLeft => lhs << (int)rhs,
+                AstExpressionOperator.BitShiftRight => lhs >> (int)rhs,
+
+                _ => throw new InvalidOperationException($"Invalid Arithmetic operator: {op}.")
+            };
+        }
+
+        private bool PerformComparisonOperation(Int64 lhs, AstExpressionOperator op, Int64 rhs)
+        {
+            return op switch
+            {
+                AstExpressionOperator.Equal => lhs == rhs,
+                AstExpressionOperator.NotEqual => lhs != rhs,
+                AstExpressionOperator.GreaterEqual => lhs >= rhs,
+                AstExpressionOperator.SmallerEqual => lhs <= rhs,
+                AstExpressionOperator.Greater => lhs > rhs,
+                AstExpressionOperator.Smaller => lhs < rhs,
+
+                _ => throw new InvalidOperationException($"Invalid Comparison operator: {op}.")
+            };
+        }
+
+        private bool PerformLogicalOperation(bool lhs, AstExpressionOperator op, bool rhs)
+        {
+            return op switch
+            {
+                AstExpressionOperator.And => lhs && rhs,
+                AstExpressionOperator.Or => lhs | rhs,
+                AstExpressionOperator.Not => !rhs,
+
+                _ => throw new InvalidOperationException($"Invalid Logical operator: {op}.")
             };
         }
 
@@ -64,7 +134,7 @@ namespace Zsharp.Semantics
 
         public override void VisitLiteralNumeric(AstLiteralNumeric numeric)
         {
-            _values.Push(numeric.Value);
+            _values.Push((Int64)numeric.Value);
         }
 
         public override void VisitLiteralString(AstLiteralString literalString)
