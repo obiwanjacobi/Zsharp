db4bceb1d4601511734fd1f22e34d7020978da0c
Marc Jacobi <marc.jacobi@macaw.nl>
2021-05-29 19:19:09 +0200

    more opt

--- src/Zsharp.Runtime/OptOfT.cs
+++ src/Zsharp.Runtime/OptOfT.cs
@@ -17,13 +17,43 @@ namespace Zsharp.Runtime
 
     public sealed class Opt<T> : Opt, IEnumerable<T>
     {
-        private readonly T _value;
-
         public Opt() : base(false) { }
         public Opt(T value) : base(true)
             => _value = value ?? throw new ArgumentNullException(nameof(value));
         public Opt(IEnumerable<T> valueOrEmpty) : base(valueOrEmpty.Any())
             => _value = valueOrEmpty.SingleOrDefault();
+
+        // Nullable interface
+        private readonly T _value;
+        public T Value
+        {
+            get
+            {
+                if (HasValue)
+                    return _value;
+                throw new InvalidOperationException("The Option has no value.");
+            }
+        }
+
+        public T GetValueOrDefault()
+            => HasValue ? _value : default;
+        public T GetValueOrDefault(T defaultValue)
+            => HasValue ? _value : defaultValue;
+
+        public bool TryGetValue(out T value)
+        {
+            if (HasValue)
+            {
+                value = _value;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        public Opt<T> Or(Opt<T> option)
+            => HasValue ? this : option;
 
         // linq support
         public Opt<R> Select<R>(Func<T, R> selector)
@@ -37,9 +67,42 @@ namespace Zsharp.Runtime
             return new Opt<R>();
         }
 
+        public IEnumerable<R> SelectMany<R>(Func<T, IEnumerable<R>> selector)
+        {
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector));
+
+            if (HasValue)
+                return selector(_value);
+
+            return new Opt<R>();
+        }
+
+        public Opt<T> Where(Func<T, bool> predicate)
+        {
+            if (predicate == null)
+                throw new ArgumentNullException(nameof(predicate));
+
+            if (HasValue && predicate(_value))
+                return this;
+
+            return new Opt<T>();
+        }
+
         // Map == Select
         public Opt<R> Map<R>(Func<T, R> selector)
             => Select<R>(selector);
+
+        public Opt<R> Bind<R>(Func<T, Opt<R>> selector)
+        {
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector));
+
+            if (HasValue)
+                return selector(_value);
+
+            return new Opt<R>();
+        }
 
         // (ternary) conditional operator works same as Match
         // val = optVal ? true : false
@@ -47,15 +110,11 @@ namespace Zsharp.Runtime
             => HasValue ? some(_value) : none;
 
         public override string ToString()
-        {
-            if (HasValue)
-                return _value.ToString();
-            return Nothing;
-        }
+            => HasValue ? _value.ToString() : Nothing;
 
         // operator overloads
         public static implicit operator Opt<T>(T value)
-            => new(value);
+            => value != null ? new(value) : new();
         public static bool operator true(Opt<T> opt)
             => opt.HasValue;
         public static bool operator false(Opt<T> opt)
